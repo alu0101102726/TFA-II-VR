fd369d19a78d0ed34372f8dc4f7ca335d4ce1f27
Carlos <alu0101102726@ull.edu.es>
2021-01-27 17:08:14 +0000

    Añadidos Delegados

---
+++ Delegates/MainDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainDelegate : MonoBehaviour
+{
+    public delegate void enemyMove();
+    public event enemyMove enemyMoveEvent;
+    public GeneralPlayer player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.enemyMoveEvent += start_movement;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void start_movement(){
+        print("estamos en el delegado");
+        if(enemyMoveEvent != null){
+            enemyMoveEvent();
+        }
+    }
+}
