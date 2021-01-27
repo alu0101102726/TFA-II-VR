5386ce1654bb585f8d7acda3be857cfe4348090b
Carlos <alu0101102726@ull.edu.es>
2021-01-27 16:57:24 +0000

    Mejora movimiento y vida enemigos

--- Enemy/EnemyHealth.cs
+++ Enemy/EnemyHealth.cs
@@ -1,21 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour {
     public int maxHealth = 100;
     public int currentHealth;
     public GameObject bullet;
+    public bool isMision;
+    public LogicaNPC logicNPC;
+    public AudioClip audioDeath;
+    public AudioClip audioHurt;
+    private AudioSource audio;
 
     void Start() {
         currentHealth = maxHealth;
+        audio = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter(Collision collision) {
-      if(currentHealth == 0) {
+      if(currentHealth <= 0) {
           Destroy(gameObject);
+          if (isMision) {
+            logicNPC.numObjetivos--;
+            logicNPC.textoMision.text = "Mata a 5 Zombunnys \n Restantes: " + logicNPC.numObjetivos;
+            if(logicNPC.numObjetivos <= 0) {
+              logicNPC.textoMision.text = "Completaste la mision";
+              logicNPC.botonMision.SetActive(true);
+            }
+          }
       }
       if(bullet.tag == collision.gameObject.tag) {
+        if(currentHealth == 20) {
+          audio.PlayOneShot(audioDeath, 0.5F);
+        }
+        else {
+          audio.PlayOneShot(audioHurt, 0.5F);
+        }
         TakeDamage(20);
       }
     }
--- Enemy/EnemyMovement.cs
+++ Enemy/EnemyMovement.cs
@@ -10,6 +10,7 @@ public class EnemyMovement : MonoBehaviour  {
         public Vector2 idleTimeRange;
         [Range(0f,1f)]
         public float psychicLevels = 0.2f;
+        public MainDelegate navStart;
 
         float currentVision; 
         Transform player;
@@ -18,118 +19,124 @@ public class EnemyMovement : MonoBehaviour  {
         NavMeshAgent nav;
         public float timer = 0f;
 
-        void Awake ()
-        {
-            player = GameObject.FindGameObjectWithTag ("Player").transform;
-            playerHealth = player.GetComponent <PlayerHealth> ();
-            enemyHealth = GetComponent <EnemyHealth> ();
-            nav = GetComponent<NavMeshAgent>();
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent<NavMeshAgent>();
+        navStart.enemyMoveEvent += StartPath;
 
-            //StartPausible();
-        }
+        // StartPath();
+    }
 
-        void OnEnable()
-        {
+    void StartPath()
+    {
+        // if(nav.enabled == false){
+
             nav.enabled = true;
             ClearPath();
             ScaleVision(1f);
             IsPsychic();
             timer = 0f;
-        }
+        // } else {
+            // nav.enabled = false;
+        // }
+    }
 
-        void ClearPath()
-        {
-            if (nav.hasPath)
-                nav.ResetPath();
-        }
+    void ClearPath()
+    {
+        if (nav.hasPath)
+            nav.ResetPath();
+    }
 
-        void Update () {
-            if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0) {
-                LookForPlayer();
-                WanderOrIdle();
-            }
-            else {
-                nav.enabled = false;
-            }
+    void Update () {
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0) {
+            LookForPlayer();
+            WanderOrIdle();
         }
-
-        void OnDestroy() {
+        else {
             nav.enabled = false;
         }
+    }
 
-        private void LookForPlayer() {
-            TestSense(player.position, currentVision);
-        }
+    void OnDestroy() {
+        nav.enabled = false;
+    }
 
-        private void HearPoint(Vector3 position) {
-            TestSense(position, hearingRange);
-        }
+    private void LookForPlayer() {
+        TestSense(player.position, currentVision);
+    }
 
-        private void TestSense(Vector3 position, float senseRange) {
-            if (Vector3.Distance(this.transform.position, position) <= senseRange)
-            {
-                GoToPosition(position);
-            }
-        }
+    private void HearPoint(Vector3 position) {
+        TestSense(position, hearingRange);
+    }
 
-        public void GoToPlayer()
+    private void TestSense(Vector3 position, float senseRange) {
+        if (Vector3.Distance(this.transform.position, position) <= senseRange)
         {
-            GoToPosition(player.position);
-        }
-
-        private void GoToPosition(Vector3 position)
-        {
-            timer = -1f;
-            //if (!enemyHealth.IsDead()) {
-                SetDestination(position);
-            //}
-        }
-
-        private void SetDestination(Vector3 position) {
-            if (nav.isOnNavMesh)  {
-                float step =  speed * Time.deltaTime;
-                transform.position = Vector3.MoveTowards(transform.position, player.position, step);
-                nav.SetDestination(position);
-            }
-        }
-
-        private void WanderOrIdle() {
-            if (!nav.hasPath) {
-                if (timer <= 0f) {
-                    SetDestination(GetRandomPoint(wanderDistance, 5));
-                    if (nav.pathStatus == NavMeshPathStatus.PathInvalid)
-                    {
-                        ClearPath();
-                    }
-                    timer = Random.Range(idleTimeRange.x, idleTimeRange.y);
-                }
-                else {
-                    timer -= Time.deltaTime;
-                }
-            }
-        }
-
-        private void IsPsychic() {
-            GoToPlayer();
-        }
-
-        private Vector3 GetRandomPoint(float distance, int layermask)  {
-            Vector3 randomPoint = UnityEngine.Random.insideUnitSphere * distance + this.transform.position;;
-
-            NavMeshHit navHit;
-            NavMesh.SamplePosition(randomPoint, out navHit, distance, layermask);
-
-            return navHit.position;
-        }
-
-        public void ScaleVision(float scale)  {
-            currentVision = visionRange * scale;
-        }
-
-        private int GetCurrentNavArea() {
-            NavMeshHit navHit;
-            nav.SamplePathPosition(-1, 0.0f, out navHit);
-
-            return navHit.mask;
+            GoToPosition(position);
         }
     }
+
+    public void GoToPlayer()
+    {
+        GoToPosition(player.position);
+    }
+
+    private void GoToPosition(Vector3 position)
+    {
+        timer = -1f;
+        //if (!enemyHealth.IsDead()) {
+            SetDestination(position);
+        //}
+    }
+
+    private void SetDestination(Vector3 position) {
+        if (nav.isOnNavMesh)  {
+            float step =  speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+            nav.SetDestination(position);
+        }
+    }
+
+    private void WanderOrIdle() {
+        if (!nav.hasPath) {
+            if (timer <= 0f) {
+                SetDestination(GetRandomPoint(wanderDistance, 5));
+                if (nav.pathStatus == NavMeshPathStatus.PathInvalid)
+                {
+                    ClearPath();
+                }
+                timer = Random.Range(idleTimeRange.x, idleTimeRange.y);
+            }
+            // else {
+            //     timer -= Time.deltaTime;
+            // }
+        }
+    }
+
+    private void IsPsychic() {
+        GoToPlayer();
+    }
+
+    private Vector3 GetRandomPoint(float distance, int layermask)  {
+        Vector3 randomPoint = UnityEngine.Random.insideUnitSphere * distance + this.transform.position;;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randomPoint, out navHit, distance, layermask);
+
+        return navHit.position;
+    }
+
+    public void ScaleVision(float scale)  {
+        currentVision = visionRange * scale;
+    }
+
+    private int GetCurrentNavArea() {
+        NavMeshHit navHit;
+        nav.SamplePathPosition(-1, 0.0f, out navHit);
+
+        return navHit.mask;
+    }
+}
